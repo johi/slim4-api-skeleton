31bd4a752025e1f7215bed79cdb972af2fe9d5fb
Johan Lerche Schulz <johan.schulz@gmail.com>
2019-12-14 13:06:03 +0100

    Added usage of http response code constants for custom Http* exceptions

--- api/src/Application/Actions/Exception/HttpConflictException.php
+++ api/src/Application/Actions/Exception/HttpConflictException.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Exception;
 
+use App\Application\Actions\Action;
 use Slim\Exception\HttpSpecializedException;
 
 class HttpConflictException extends HttpSpecializedException
 {
-    protected $code = 409;
+    protected $code = Action::HTTP_CONFLICT;
     protected $message = 'Conflict';
     protected $title = '409 Conflict';
     protected $description = 'The server encountered a conflict for this request';
--- api/src/Application/Actions/Exception/HttpNotAcceptableException.php
+++ api/src/Application/Actions/Exception/HttpNotAcceptableException.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Exception;
 
+use App\Application\Actions\Action;
 use Slim\Exception\HttpSpecializedException;
 
 class HttpNotAcceptableException extends HttpSpecializedException
 {
-    protected $code = 406;
+    protected $code = Action::HTTP_NOT_ACCEPTABLE;
     protected $message = 'Not acceptable';
     protected $title = '406 Not acceptable';
     protected $description = 'The server could not process the request as preconditions are not met';
